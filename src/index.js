1d96c4987aeb6372fee50184d32f20ceaa92381e
HillaryChumacero <yamille.chumacero@gmail.com>
2018-06-04 12:35:15 -0500

    agregando pry final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const btnContinue = document.getElementById("btnContinue");
-btnContinue.addEventListener("click",() => {
+btnContinue.addEventListener("click", () => {
   const hidden = document.getElementById("root");
   const show = document.getElementById("sec_cipher");
   hidden.style.display = "none";
@@ -7,45 +7,19 @@ btnContinue.addEventListener("click",() => {
 });
 
 /********************/
-document.getElementById("btnCifrarTexto").addEventListener("click", () =>{
+document.getElementById("btnCifrarTexto").addEventListener("click", () => {
   let string = document.getElementById("text_cip").value;
   let offset = parseInt(document.getElementById("offset").value);
   let text_result = document.getElementById("text_result");
-text_result.innerHTML = cipher.encode(offset, string);
+  text_result.innerHTML = cipher.encode(offset, string);
 });
 /********************/
-document.getElementById("btnDecifrarTexto").addEventListener("click", () =>{
+document.getElementById("btnDecifrarTexto").addEventListener("click", () => {
   let string = document.getElementById("text_cip").value;
   let offset = parseInt(document.getElementById("offset").value);
   let text_result = document.getElementById("text_result");
   text_result.innerHTML = cipher.decode(offset, string);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("btnCifrarTexto").addEventListener("click", () =>{
-//   const string = document.getElementById("string").value;
-//   const offset = parseInt(document.getElementById("offset").value);
-//   const text_result = document.getElementById("text_result");
-// text_result.innerHTML = cipher.encode(offset, string);
-// });
-// /********************/
-// document.getElementById("btnDecifrarTexto").addEventListener("click", () =>{
-//   const string = document.getElementById("string").value;
-//   const offset = parseInt(document.getElementById("offset").value);
-//   const text_result = document.getElementById("text_result");
-//   text_result.innerHTML = cipher.decode(offset, string);
-// });
 
 /********************/
 //BOTON COPIAR TEXTO
@@ -58,87 +32,3 @@ document.getElementById("btnCopyText").addEventListener("click", () => {
   document.body.removeChild(x);
 })
 
-/********************/
-//BUTTON CIFRAR-TEXTO
-// const btnCifrarTexto = document.getElementById("btnCifrarTexto");
-// btnCifrarTexto.addEventListener("click",resultCifrarTexto);
-// function resultCifrarTexto(){ debugger
-// const ocultocif =    document.getElementById("container-back-encode");
-// const ocultodescif = document.getElementById("container-back-decode");
-// const muestra=       document.getElementById('container-result');
-
-// muestra.style.display = "block";
-// ocultocif.style.display="none";
-// ocultodescif.style.display="none";
-
-// var Numero= document.getElementById('offset').value;
-// var Text= document.getElementById('text_cif').value;
-
-// alert(""+Numero);
-// alert(""+Text);
-
-// var respuesta = encode(Text);
-// alert(respuesta);
-
-// }
-
-
-
-// //BUTTON DESCIFRAR-TEXTO
-// const btnDescifrarTexto = document.getElementById("btnDescifrarTexto");
-// btnDescifrarTexto.addEventListener("click",resultDescifrarTexto);
-// function resultDescifrarTexto(){ debugger
-// const ocultocif =    document.getElementById("container-back-encode");
-// const ocultodescif = document.getElementById("container-back-decode");
-// const muestra=       document.getElementById('container-result');
-
-// muestra.style.display = "block";
-// ocultocif.style.display="none";
-// ocultodescif.style.display="none";
-
-// var Numero=       document.getElementById('offset').value;
-// var Text=       document.getElementById('text_descif').value;
-
-// alert(""+Numero);
-// alert(""+Text);
-
-// var respuesta = decode(Text);
-// alert(respuesta);
-
-// }
-// //RESULTADO
-
-
-// /////
-//   var offset = document.getElementById("offset");
-//   var resultadotext= "";
-
-// function encode(texto){ 
-//   var resultadotext="";
-//   for(var i=0; i< texto.length; i++){
-//     resultadotext += texto.charCodeAt(i)+" ";
-//   }
-//   return resultadotext.trim();
-// }
-
-// function decode(texto){
-//   var textosplit=texto.split(" ");
-//   var temp = new Array();
-//   temp = textosplit(" ");
-
-//   for (a in temp ) {
-//     temp[a] = parseInt(temp[a], 10); 
-//   }
-
-//   for(var i=0; i< temp.length; i++){
-//     resultadotext += texto.charCodeAt(i)+" ";
-//   }
-//   var resultadotext = String.fromCharCode(textosplit);
-//   return resultadotext.trim();
-
-// }
-// // let string, offset;
-
-// //    //////para ocultar y mostrar un div
-// // <a href="#" onclick="ocultar()">Oculta la capa 1</a>
-// //     <a href="#" onclick="mostrar()">Muestra la capa 1</a>
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -11,7 +11,28 @@ describe('cipher', () => {
     });
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
-      assert.equal(cipher.createCipherWithOffset(33).encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+      assert.equal(cipher.createCipherWithOffset(33).encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+  });
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () => {
+      assert.equal(cipher.createCipherWithOffset(33).encode("abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");
+    });
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar " " para " " con offest 33', () => {
+      assert.equal(cipher.createCipherWithOffset(33).encode(" "), " ");
     });
   });
 
@@ -20,9 +41,30 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
-//
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',() => {
-      assert.equal(cipher.createCipherWithOffset(33).decode("HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    //
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      assert.equal(cipher.createCipherWithOffset(33).decode("HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+  });
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () => {
+      assert.equal(cipher.createCipherWithOffset(33).decode("hijklmnopqrstuvwxyzabcdefg"), "abcdefghijklmnopqrstuvwxyz");
+    });
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar " " para " " con offest 33', () => {
+      assert.equal(cipher.createCipherWithOffset(33).decode(" "), " ");
     });
   });
 
@@ -34,7 +76,7 @@ describe('cipher', () => {
 
     it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado', () => {
       assert.isObject(cipher.createCipherWithOffset(33));
-      assert.property(cipher.createCipherWithOffset(33),'encode');
+      assert.property(cipher.createCipherWithOffset(33), 'encode');
       assert.property(cipher.createCipherWithOffset(33), 'decode');
     });
 
